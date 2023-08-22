fdc006ed8c7de71bf5007683fffc028e5f79b269
Con Mewww <90909432+tantd2203@users.noreply.github.com>
2023-08-22 23:16:29 +0700

    docker

--- console.js
+++ console.js
@@ -1,3 +1,13 @@
 console.log("hello")
 console.log("hello")
 console.log("hello")
+version: '3.9'
+
+services:
+  redis-stack:
+    image: redis/redis-stack-server:latest
+    container_name: sbc_ciam_redis
+    ports:
+      - 6379:6379
+    #environment:
+      #REDIS_ARGS: "--requirepass jav56xkNbpsmLDeUr9GF"
